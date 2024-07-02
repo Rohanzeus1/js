394db1bbd81378179e298802a6d3aa44ce6b87dc
Rohanzeus1 <164084780+Rohanzeus1@users.noreply.github.com>
2024-07-02 18:44:02 +0000

    datatype

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const accountNumber=23332
+let accountEmail="rohan.17saxena@gmail.com"
+var accountPassword="Rohan@17"
+accountCity="Lucknow"
+console.table([accountEmail,accountNumber,accountPassword,accountCity])
